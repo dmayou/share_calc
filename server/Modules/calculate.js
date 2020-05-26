378ebd4e774d92f20bc6293071276fd224d1a997
David Mayou <davidpmayou@gmail.com>
2020-05-26 18:41:25 -0500

    stub calculate module, add unit tests

--- server/Modules/calculate.js
+++ server/Modules/calculate.js
@@ -3,11 +3,12 @@ const calculate = (expression) => {
         return 'err';
     }
     // get first operand
-    const operatorsReg = /[+\{u00f7}\u{00d7}\u{2212}]/;
+    const operatorsReg = /[+\{u00f7}\u{00d7}\u{2212}]/; // "+ / * -""
     const { index, input: operator } = expression.match(operatorsReg);
     console.log('result:', index, operator);
-
-    return '';
+    const firstOperand = expression.slice(0, index);
+    const secondOperand = expression.slice(index);
+    return firstOperand + secondOperand;
 }
 
 module.exports = calculate;
