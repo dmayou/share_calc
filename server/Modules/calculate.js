18e1a37b445de5e7ad9beaedc84321a046e00983
David Mayou <davidpmayou@gmail.com>
2020-05-26 18:59:45 -0500

    handle single-digit addition

--- server/Modules/calculate.js
+++ server/Modules/calculate.js
@@ -2,13 +2,22 @@ const calculate = (expression) => {
     if (expression === '' || expression === null) {
         return 'err';
     }
-    // get first operand
-    const operatorsReg = /[+\{u00f7}\u{00d7}\u{2212}]/; // "+ / * -""
-    const { index, input: operator } = expression.match(operatorsReg);
-    console.log('result:', index, operator);
-    const firstOperand = expression.slice(0, index);
-    const secondOperand = expression.slice(index);
-    return firstOperand + secondOperand;
+    // find operator
+    const operatorsReg = /[+\u{00f7}\u{00d7}\u{2212}]/u; // "+ / * -""
+    const { 0: operator, index } = expression.match(operatorsReg);
+    // find operands
+    const firstOperand = Number(expression.slice(0, index));
+    const secondOperand = Number(expression.slice(index + 1));
+
+    let result = 'err';
+    switch (operator) {
+        case '+':
+            result = String(firstOperand + secondOperand);
+            break;
+        default:
+            console.error('Error: Invalid operator', operator);
+    }
+    return result;
 }
 
 module.exports = calculate;
