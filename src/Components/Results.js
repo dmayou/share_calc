d7f4ac26ec25318cc1a4a8f2dac1d627833ba5f7
David Mayou <davidpmayou@gmail.com>
2020-05-27 15:41:16 -0500

    format Results component as table

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './Results.css';
 
-function Results() {
+function Results({ results }) {
+    const resultsDisplay = results.map( (result, i) => {
+        return (
+            <tr key={i}>
+                <td>
+                    {result}
+                </td>
+            </tr>
+        );
+    }) 
     return (
         <div className="container Results">
-            1 + 1 = 2<br />
-            2 x 2 = 4<br />
-            1763 - 999 = 764<br />
-            18 / 6 = 3<br />
-            5678 * 2 = 12345<br />
-            1 + 1 = 2<br />
-            2 x 2 = 4<br />
-            1763 - 999 = 764<br />
-            18 / 6 = 3<br />
-            5678 * 2 = 12345<br />
-            
+            <table>
+                <tbody>
+                    {resultsDisplay || ''}
+                </tbody>
+            </table>
         </div>
     );
 }
