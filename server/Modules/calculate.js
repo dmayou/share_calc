eadd6dbd8d070d7d1bf54bf95f572cd98c713a31
David Mayou <davidpmayou@gmail.com>
2020-05-26 20:29:24 -0500

    add all operations, with unit tests

--- server/Modules/calculate.js
+++ server/Modules/calculate.js
@@ -2,22 +2,31 @@ const calculate = (expression) => {
     if (expression === '' || expression === null) {
         return 'err';
     }
-    // find operator
+    // parse operator
     const operatorsReg = /[+\u{00f7}\u{00d7}\u{2212}]/u; // "+ / * -""
-    const { 0: operator, index } = expression.match(operatorsReg);
-    // find operands
-    const firstOperand = Number(expression.slice(0, index));
-    const secondOperand = Number(expression.slice(index + 1));
-
+    const { 0: operator, index=0 } = expression.match(operatorsReg) || {};
+    // parse operands
+    const operand1 = Number(expression.slice(0, index));
+    const operand2 = Number(expression.slice(index + 1));
+    // perform calculation
     let result = 'err';
     switch (operator) {
-        case '+':
-            result = String(firstOperand + secondOperand);
+        case '+': // addition
+            result = operand1 + operand2;
+            break;
+        case '\u{00f7}': // division
+            result = operand1 / operand2;
+            break;
+        case '\u{00d7}': // multiplication
+            result = operand1 * operand2;
+            break;
+        case '\u{2212}': // subtraction
+            result = operand1 - operand2;
             break;
         default:
             console.error('Error: Invalid operator', operator);
     }
-    return result;
+    return String(result);
 }
 
 module.exports = calculate;
--- server/Modules/calculate.test.js
+++ server/Modules/calculate.test.js
@@ -3,15 +3,22 @@ const calculate = require ('./calculate');
 const tests = [
     { descr: `return 'err' for null`, input: null, result: 'err' },
     { descr: `return 'err' for empty`, input: '', result: 'err' },
+    { descr: `handle garbage input`, input: 'gsflkjh;lrtoiu', result: 'err' },
     { descr: `calculate single-digit addition`, input: '2+3', result: '5' },
     { descr: `calculate multi-digit addition`, input: '123+456', result: '579' },
+    { descr: `calculate decimal addition`, input: '1.23+4.5', result: '5.73' },
+    { descr: `calculate division`, input: '121\u{00f7}11', result: '11' },
+    { descr: `calculate decimal division`, input: '15.25\u{00f7}16.9', result: String(15.25/16.9) },
+    { descr: `return Infinity for divide by 0`, input: '121\u{00f7}0', result: 'Infinity' },
+    { descr: `calculate multiplication`, input: '256\u{00d7}16', result: String(256 * 16) },
+    { descr: `calculate subtraction`, input: '123.456\u{2212}765.4', result: String(123.456 - 765.4) },
     // { descr: ``, input: , result: '' },
 ];
 
 describe("Calculation Checks", () => {
-    it("should return true", () => {
-        expect(1+1).toEqual(2);
-    });
+    // it("should calculate sanity check correctly", () => {
+    //     expect(1+1).toEqual(2);
+    // });
     for (let { descr, input, result } of tests) {
         it(`should ${descr}`, () => {
             expect(calculate(input)).toEqual(result);
