a6b5a1b0223d7715870140a10a86c9f4f49541e2
David Mayou <davidpmayou@gmail.com>
2020-05-27 16:47:36 -0500

    scale display based on expression length

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -2,10 +2,25 @@ import React from 'react';
 import './Display.css';
 
 function Display({ expression }) {
+    const length = expression.length;
+    let fontReduction;
+    if (length > 10) {
+        fontReduction = .2;
+    }
+    if (length > 16) {
+        fontReduction = .4;
+    }
+    if (length > 20) {
+        fontReduction = .6;
+    }
+    const style = {
+        fontSize: 1 - fontReduction + 'em',
+    };
+    console.log(style);
     return (
         <div className="Display">
             <div className="bezel">
-                <div className="expression">
+                <div className="expression" style={style}>
                     {expression}
                 </div>
             </div>
