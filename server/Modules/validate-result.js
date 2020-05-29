1610d2f82289f196041b566f7d0367028ee6bf1c
David Mayou <davidpmayou@gmail.com>
2020-05-29 12:17:02 -0500

    add validateResult check to server.js

--- server/Modules/validate-result.js
+++ server/Modules/validate-result.js
@@ -1,30 +1,30 @@
 const validateResult = equation => {
-// valid result is in the form of an equation:
-// <operand><operator><operand>=<result>
-// general strategy:
-//   - make general checks (length, presence of operator, equals)
-//   - remove all valid characters and patterns, then see if 
-//     anything is left
+    // valid result is in the form of an equation:
+    // <operand><operator><operand>=<result>
+    // general strategy:
+    //   - make general checks (length, presence of operator, equals)
+    //   - remove all valid characters and patterns, then see if 
+    //     anything is left
 
-// will still allow wrong things through, but seriously 
-// limits html injection
+    // will still allow wrong things through, but seriously 
+    // limits html injection
 
-const operatorReg = /[+\u{00f7}\u{00d7}\u{2212}]/u; // + / * -
+    const operatorReg = /[+\u{00f7}\u{00d7}\u{2212}]/u; // + / * -
 
-if (equation === '' // blank is invalid
-    || !equation.includes('=') // must contain =
-    || !operatorReg.test(equation) // must have an operator
-    ) {
-    return false;
-}
+    if (equation === '' // blank is invalid
+        || !equation.includes('=') // must contain =
+        || !operatorReg.test(equation) // must have an operator
+        ) {
+        return false;
+    }
 
-// remove legal patterns and characters
-let str = equation
-    .replace('NaN', '')
-    .replace('Infinity', '')
-    .replace(operatorReg, '')
-    .replace(/[0-9.]/g, '')    // numerals and radix points
-    .replace('=', '');         // single '='
+    // remove legal patterns and characters
+    let str = equation
+        .replace('NaN', '')
+        .replace('Infinity', '')
+        .replace(operatorReg, '')
+        .replace(/[0-9.]/g, '')    // numerals and radix points
+        .replace('=', '');         // single '='
 
     return str === ''; // valid if nothing left
 };
